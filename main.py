d0ca2c3e9faa97e358f796f37864e9d88e2888b2
Andrew Buchan <andyhasit@gmail.com>
2021-09-27 23:01:31 +0100

    Allow specifying of individual targets

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from config import load_config
 from exceptions import FatalException
 
@@ -5,7 +6,14 @@ from exceptions import FatalException
 if __name__ == '__main__':
     try:
         config = load_config()
-        for target in config.targets:
+        if len(sys.argv) > 1:
+            specified_targets = sys.argv[1:]
+        if specified_targets:
+            targets_to_backup = [t for t in config.targets if t.name in specified_targets]
+        else:
+            targets_to_backup = config.targets
+        for target in targets_to_backup:
+            print('-' * 80)
             target.run()  # TODO: use threading
     except FatalException as e:
         print(e)
